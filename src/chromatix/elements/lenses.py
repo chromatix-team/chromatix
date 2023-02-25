c5aa55dabc4e8599e776c954201fe736f92fc87f
Diptodip Deb <dd@d2d.sh>
2023-02-25 02:49:34 -0500

    Add docstrings for elements/lenses

--- src/chromatix/elements/lenses.py
+++ src/chromatix/elements/lenses.py
@@ -8,6 +8,23 @@ __all__ = ["ThinLens", "FFLens", "DFLens"]
 
 
 class ThinLens(nn.Module):
+    """
+    Applies a thin lens placed directly after the incoming ``Field``.
+    This element returns the ``Field`` directly after the lens.
+
+    This element can be placed after any element that returns a ``Field`` or
+    before any element that accepts a ``Field``.
+
+    The attributes ``f``, ``n``, and ``NA`` can be learned by using
+    ``chromatix.utils.trainable``.
+
+    Attributes:
+        f: Focal length of the lens.
+        n: Refractive index of the lens.
+        NA: If provided, the NA of the lens. By default, no pupil is applied
+            to the incoming ``Field``.
+    """
+
     f: Union[float, Callable[[PRNGKey], float]]
     n: Union[float, Callable[[PRNGKey], float]]
     NA: Optional[Union[float, Callable[[PRNGKey], float]]] = None
@@ -24,6 +41,23 @@ class ThinLens(nn.Module):
 
 
 class FFLens(nn.Module):
+    """
+    Applies a thin lens placed a distance ``f`` after the incoming ``Field``.
+    This element returns the ``Field`` a distance ``f`` after the lens.
+
+    This element can be placed after any element that returns a ``Field`` or
+    before any element that accepts a ``Field``.
+
+    The attributes ``f``, ``n``, and ``NA`` can be learned by using
+    ``chromatix.utils.trainable``.
+
+    Attributes:
+        f: Focal length of the lens.
+        n: Refractive index of the lens.
+        NA: If provided, the NA of the lens. By default, no pupil is applied
+            to the incoming ``Field``.
+    """
+
     f: Union[float, Callable[[PRNGKey], float]]
     n: Union[float, Callable[[PRNGKey], float]]
     NA: Optional[Union[float, Callable[[PRNGKey], float]]] = None
@@ -40,6 +74,24 @@ class FFLens(nn.Module):
 
 
 class DFLens(nn.Module):
+    """
+    Applies a thin lens placed a distance ``d`` after the incoming ``Field``.
+    This element returns the ``Field`` a distance ``f`` after the lens.
+
+    This element can be placed after any element that returns a ``Field`` or
+    before any element that accepts a ``Field``.
+
+    The attributes ``d``, ``f``, ``n``, and ``NA`` can be learned by using
+    ``chromatix.utils.trainable``.
+
+    Attributes:
+        d: Distance from the incoming ``Field`` to the lens.
+        f: Focal length of the lens.
+        n: Refractive index of the lens.
+        NA: If provided, the NA of the lens. By default, no pupil is applied
+            to the incoming ``Field``.
+    """
+
     d: Union[float, Callable[[PRNGKey], float]]
     f: Union[float, Callable[[PRNGKey], float]]
     n: Union[float, Callable[[PRNGKey], float]]
