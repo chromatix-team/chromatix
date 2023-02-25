53fb9628284ded17ea5df96636fed50c6ba0d1ef
Diptodip Deb <dd@d2d.sh>
2023-02-25 02:51:26 -0500

    Add docstring for elements/propagation

--- src/chromatix/elements/propagation.py
+++ src/chromatix/elements/propagation.py
@@ -8,6 +8,43 @@ __all__ = ["Propagate"]
 
 
 class Propagate(nn.Module):
+    """
+    Free space propagation that can be placed after or between other elements.
+
+    This element takes a ``Field`` as input and outputs a ``Field`` that has
+    been propagated by a distance ``z``. Optionally, the index of refraction of
+    the propagation medium can be learned.
+
+    For example, if this element is constructed as:
+
+    ```python
+    from chromatix.elements import Propagate
+    Propagate(n=1.33, method='transfer', mode='same')
+    ```
+
+    then this element has no trainable parameters, but if this element is
+    constructed as:
+
+    ```python
+    from chromatix.elements import Propagate
+    from chromatix.utils import trainable
+    Propagate(n=trainable(1.33), method='transfer', mode='same')
+    ```
+
+    then this element has a trainable refractive index, initialized to 1.33.
+
+    For more details, see ``chromatix.functional.propagate``.
+
+    Attributes:
+        n: Refractive index.
+        N_pad: If provided, the padding for propagation (will be used as both
+            height and width padding). Otherwise, the padding will be
+            automatically calculated in ``chromatix.functional.propagate``.
+        method: The propagation method for ``chromatix.functional.propagate``.
+        mode: Defines the cropping of the output in
+            ``chromatix.functional.propagate``.
+    """
+
     n: Union[float, Callable[[PRNGKey], float]]
     N_pad: Optional[int] = None
     method: str = "transfer"
