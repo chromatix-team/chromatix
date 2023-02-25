d878bc9f7d6215c910ad8f8dc659816e4ce64cb1
Diptodip Deb <dd@d2d.sh>
2023-02-25 03:07:07 -0500

    Add parallelization examples

---
+++ examples/parallel_imaging.py
@@ -0,0 +1,86 @@
+from chromatix.elements import ObjectivePointSource, PhaseMask, FFLens
+from chromatix import Microscope, OpticalSystem
+from chromatix.utils import trainable
+from chromatix.functional.phase_masks import flat_phase
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax import linen as nn
+from functools import partial
+from time import perf_counter_ns
+
+shape = (1536, 1536) # number of pixels in simulated field
+spacing = 0.3 # spacing of pixels for the final PSF, microns
+spectrum = 0.532 # microns
+spectral_density = 1.0
+f = 100.0 # focal length, microns
+n = 1.33 # refractive index of medium
+NA = 0.8 # numerical aperture of objective
+
+microscope = Microscope(
+    optical_system=[
+        ObjectivePointSource(shape, spacing, spectrum, spectral_density, f, n, NA),
+        PhaseMask(trainable(flat_phase)),
+        FFLens(f, n)
+    ],
+    reduce_fn=lambda image: jnp.sum(image, axis=0),
+)
+
+def init_params(key, volume, z):
+    params = microscope.init(key, volume, z)
+    return params
+
+@jax.jit
+def compute_image(params, volume, z):
+    return microscope.apply(params, volume, z)
+
+volume = jnp.ones((128, *shape, 1)) # fill in your volume here
+z = jnp.linspace(-4, 4, num=128)
+params = init_params(jax.random.PRNGKey(6022), volume, z)
+widefield_image = compute_image(params, volume, z)
+print(f"image has shape: {widefield_image.shape}")
+
+single_gpu_times = []
+for i in range(10):
+    print(i)
+    start = perf_counter_ns()
+    _ = compute_image(params, volume, z).block_until_ready()
+    end = perf_counter_ns()
+    single_gpu_times.append((end - start) / 1e6)
+
+print(f"single gpu: {np.mean(single_gpu_times)} +/- {np.std(single_gpu_times)} ms")
+
+microscope = Microscope(
+    optical_system=[
+        ObjectivePointSource(shape, spacing, spectrum, spectral_density, f, n, NA),
+        PhaseMask(trainable(flat_phase)),
+        FFLens(f, n)
+    ],
+    reduce_fn=lambda image: jax.lax.psum(jnp.sum(image, axis=0), axis_name='devices'),
+)
+
+@partial(jax.pmap, axis_name='devices')
+def init_params(key, volume, z):
+    params = microscope.init(key, volume, z)
+    return params
+
+@partial(jax.pmap, axis_name='devices')
+def compute_image(params, volume, z):
+    return microscope.apply(params, volume, z)
+
+volume = jnp.ones((128, *shape, 1)).reshape(4, 32, *shape, 1) # fill in your volume here
+z = jnp.linspace(-4, 4, num=128).reshape(4, 32)
+params = init_params(jax.random.split(jax.random.PRNGKey(6022), 4), volume, z)
+widefield_image = compute_image(params, volume, z)
+print(f"image has shape: {widefield_image.shape}")
+assert(jnp.all(widefield_image[0] == widefield_image[1]))
+
+pmap_gpu_times = []
+for i in range(10):
+    print(i)
+    start = perf_counter_ns()
+    _ = compute_image(params, volume, z).block_until_ready()
+    end = perf_counter_ns()
+    pmap_gpu_times.append((end - start) / 1e6)
+
+print(f"pmap multi gpu: {np.mean(pmap_gpu_times)} +/- {np.std(pmap_gpu_times)} ms")
---
+++ examples/parallel_psf.py
@@ -0,0 +1,58 @@
+from chromatix.elements import ObjectivePointSource, PhaseMask, FFLens
+from chromatix import OpticalSystem
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax import linen as nn
+from time import perf_counter_ns
+
+shape = (1536, 1536) # number of pixels in simulated field
+spacing = 0.3 # spacing of pixels for the final PSF, microns
+spectrum = 0.532 # microns
+spectral_density = 1.0
+f = 100.0 # focal length, microns
+n = 1.33 # refractive index of medium
+NA = 0.8 # numerical aperture of objective
+optical_model = OpticalSystem(
+    [
+        ObjectivePointSource(shape, spacing, spectrum, spectral_density, f, n, NA),
+        PhaseMask(jnp.ones(shape)[jnp.newaxis, ..., jnp.newaxis]),
+        FFLens(f, n)
+    ]
+)
+
+@jax.jit
+def compute_psf(z):
+    return optical_model.apply({}, z).intensity
+
+
+z = jnp.linspace(-4, 4, num=128)
+widefield_psf = compute_psf(z)
+
+single_gpu_times = []
+for i in range(10):
+    print(i)
+    start = perf_counter_ns()
+    _ = compute_psf(z).block_until_ready()
+    end = perf_counter_ns()
+    single_gpu_times.append((end - start) / 1e6)
+
+print(f"single gpu: {np.mean(single_gpu_times)} +/- {np.std(single_gpu_times)} ms")
+
+@jax.pmap
+def compute_psf(z):
+    return optical_model.apply({}, z).intensity
+
+
+z = jax.device_put_sharded([chunk_z for chunk_z in jnp.linspace(-4, 4, num=128).reshape(4, 32)], jax.devices())
+widefield_psf = compute_psf(z)
+
+pmap_gpu_times = []
+for i in range(10):
+    print(i)
+    start = perf_counter_ns()
+    _ = compute_psf(z).block_until_ready()
+    end = perf_counter_ns()
+    pmap_gpu_times.append((end - start) / 1e6)
+
+print(f"pmap multi gpu: {np.mean(pmap_gpu_times)} +/- {np.std(pmap_gpu_times)} ms")
