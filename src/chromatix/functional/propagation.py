4c5b713b07e6ea253ae33185a4a89bc4fb08a10f
Xi Yang <yang96nicole@gmail.com>
2023-03-02 12:01:24 -0500

    Modify the propagation for vector field.

--- src/chromatix/functional/propagation.py
+++ src/chromatix/functional/propagation.py
@@ -76,21 +76,37 @@ def transfer_propagate(
     L = jnp.sqrt(jnp.complex64(field.spectrum * z / n))  # lengthscale L
     # TODO(dd): This calculation could probably go into Field
     f = []
-    for d in range(field.dx.size):
-        f.append(jnp.fft.fftfreq(field.shape[1] + N_pad, d=field.dx[..., d].squeeze()))
+    if field.u.ndim > 4:
+        for d in range(field.dx.size):
+                f.append(jnp.fft.fftfreq(field.shape[2] + N_pad, d=field.dx[..., d].squeeze()))
+    else:
+        for d in range(field.dx.size):
+                f.append(jnp.fft.fftfreq(field.shape[1] + N_pad, d=field.dx[..., d].squeeze())) 
     f = jnp.stack(f, axis=-1)
-    fx, fy = rearrange(f, "h c -> 1 h 1 c"), rearrange(f, "w c -> 1 1 w c")
-    phase = -jnp.pi * L**2 * (fx**2 + fy**2)
+
+    if field.u.ndim > 4:
+        fx, fy = rearrange(f, "h c -> 1 1 h 1 c"), rearrange(f, "w c -> 1 1 1 w c")
+        u = center_pad(field.u, [0, 0, int(N_pad / 2), int(N_pad / 2), 0])
+        print("fx shape is:", fx.shape)
+        phase = -jnp.pi * L**2 * (fx**2 + fy**2)
+        print(phase.shape)
+    else:
+        fx, fy = rearrange(f, "h c -> 1 h 1 c"), rearrange(f, "w c -> 1 1 w c")
+        u = center_pad(field.u, [0, int(N_pad / 2), int(N_pad / 2), 0])
+        phase = -jnp.pi * L**2 * (fx**2 + fy**2)
+        print(phase.shape)
 
     # Propagating field
-    u = center_pad(field.u, [0, int(N_pad / 2), int(N_pad / 2), 0])
     u = ifft(fft(u, loop_axis) * jnp.exp(1j * phase), loop_axis)
 
     # Cropping output field
     if mode == "full":
         field = field.replace(u=u)
     elif mode == "same":
-        u = center_crop(u, [0, int(N_pad / 2), int(N_pad / 2), 0])
+        if field.u.ndim > 4:
+            u = center_crop(u, [0, 0, int(N_pad / 2), int(N_pad / 2), 0])
+        else:
+            u = center_crop(u, [0, int(N_pad / 2), int(N_pad / 2), 0])
         field = field.replace(u=u)
     else:
         raise NotImplementedError('Only "full" and "same" are supported.')
