0ecd52444a238fa4fa4d90c3f3a958c5713236c3
Diptodip Deb <dd@d2d.sh>
2023-03-03 00:46:35 -0500

    Change sensors to take Fields

--- src/chromatix/elements/sensors.py
+++ src/chromatix/elements/sensors.py
@@ -1,25 +1,37 @@
+import jax.numpy as jnp
 import flax.linen as nn
-from typing import Literal, Optional
+from jax import vmap
+from jax.lax import psum
+from typing import Literal, Optional, Tuple
 from chex import PRNGKey, Array
 from chromatix import Field
-from ..functional.sensors import shift_invariant_sensor
+from ..ops import init_plane_resample, approximate_shot_noise, shot_noise
 
 
-class ShiftInvariantSensor(nn.Module):
-    pixel_pitch: float  # TODO(dd): Resample to pixel pitch if needed
-    shot_noise_mode: Optional[Literal["approximate", "poisson"]] = None
-    reduce_batch: bool = True
-    parallel_axis_name: Optional[str] = None
+class ShotNoiseSensor(nn.Module):
+    shape: Tuple[int, ...]
+    spacing: float
+    shot_noise_mode: Optional[Literal['approximate', 'poisson']] = None
+    resampling_method: Optional[str] = 'linear'
+    reduce_axis: Optional[int] = None
+    reduce_parallel_axis_name: Optional[str] = None
 
-    @nn.compact
-    def __call__(self, sample: Array, psf: Array) -> Array:
-        noise_key = None if not self.shot_noise_mode else self.make_rng("noise")
-        image = shift_invariant_sensor(
-            sample,
-            psf,
-            noise_key,
-            self.shot_noise_mode,
-            self.reduce_batch,
-            self.parallel_axis_name,
-        )
+    def setup(self):
+        if self.resampling_method is not None:
+            self.resample = init_plane_resample(
+                (*self.shape, 1), self.spacing, self.resampling_method
+            )
+
+    def __call__(self, field: Field) -> Array:
+        noise_key = self.make_rng("noise")
+        if self.resampling_method is not None:
+            image = vmap(self.resample, in_axes=(0, None))(field.intensity, field.dx)
+        if self.reduce_axis is not None:
+            image = jnp.sum(image, axis=self.reduce_axis)
+        if self.reduce_parallel_axis_name is not None:
+            image = psum(image, axis_name=self.reduce_parallel_axis_name)
+        if self.shot_noise_mode == 'approximate':
+            image = approximate_shot_noise(noise_key, image)
+        elif self.shot_noise_mode == 'poisson':
+            image = shot_noise(noise_key, image)
         return image
