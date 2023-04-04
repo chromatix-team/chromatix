c11f830def83622fc5b215902a8c0766218ebed1
Gert-Jan <gertjan.both@protonmail.com>
2023-04-04 16:46:13 +0200

    Add tests on linear polariser.

--- src/chromatix/functional/polarizers.py
+++ src/chromatix/functional/polarizers.py
@@ -1,30 +1,36 @@
+from ..field import VectorField
+from typing import Union
+from chex import Array
 import jax.numpy as jnp
-
-from ..field import Field, VectorField
-from einops import rearrange
 
 __all__ = ["linear_polarizer", "left_circular_polarizer", "right_circular_polarizer"]
 
 
-def field_after_polarizer(field: VectorField, J00, J01, J10, J11) -> VectorField:
-    LP = jnp.array([[J00, J01, 0], [J10, J11, 0], [0, 0, 0]])
-    LP = LP[::-1, ::-1]  # why the inverse?
-    return field.replace(u=jnp.dot(field.u, LP.T))
+def field_after_polarizer(
+    field: VectorField,
+    J00: Union[float, complex, Array],
+    J01: Union[float, complex, Array],
+    J10: Union[float, complex, Array],
+    J11: Union[float, complex, Array],
+) -> VectorField:
+    # Invert the axes as our order is zyx
+    LP = jnp.array([[0, 0, 0], [0, J11, J10], [0, J01, J00]])
+    return field.replace(u=jnp.dot(field.u, LP))
 
 
-def linear_polarizer(field: VectorField, polarizer_angle: float) -> VectorField:
+def linear_polarizer(field: VectorField, angle: float) -> VectorField:
     """
-    Applies a thin polarizer placed directly after the incoming ``Field``.
+    Applies a thin polarizer with of a given angle w.r.t. the horizontal to the
+    incoming ``VectorField``.
 
     Args:
-        field: The ``Field`` to which the polarizer will be applied.
-        p_angle: linear polarizers oriented to pass light polarized at polarizer_angle
+        field: The ``VectorField`` to which the polarizer will be applied.
+        angle: polarizer_angle w.r.t horizontal.
 
     Returns:
-        The ``Field`` directly after the polarizer.
+        The ``VectorField`` directly after the polarizer.
     """
-    c = jnp.cos(polarizer_angle)
-    s = jnp.sin(polarizer_angle)
+    c, s = jnp.cos(angle), jnp.sin(angle)
     J00 = c**2
     J11 = s**2
     J01 = s * c
--- tests/test_polarizer.py
+++ tests/test_polarizer.py
@@ -3,21 +3,37 @@ import chromatix.functional as cf
 from chromatix import VectorField
 from functools import partial
 from chex import assert_axis_dimension
+import pytest
 
 
-def test_linear_polarizer():
+@pytest.mark.parametrize(
+    ["E0", "angle", "power"],
+    [
+        (jnp.array([0, 1, 0]), 0, 0),
+        (jnp.array([0, 1, 0]), 1 / 4 * jnp.pi, jnp.cos(1 / 4 * jnp.pi) ** 2),
+        (jnp.array([0, 1, 0]), 1 / 2 * jnp.pi, 1),
+        (jnp.array([0, 0, 1]), 0, 1),
+        (jnp.array([0, 0, 1]), 1 / 4 * jnp.pi, jnp.cos(1 / 4 * jnp.pi) ** 2),
+        (jnp.array([0, 0, 1]), 1 / 2 * jnp.pi, 0),
+    ],
+)
+def test_linear_polarizer(E0, angle, power):
     field = VectorField.create(1.0, 0.532, 1.0, shape=(512, 512))
 
-    k = jnp.array([0.0, 1.0])
-    E0 = jnp.ones((3,))
+    k = jnp.array([0.0, 0.0])
 
+    # Vertical polarized light
     field = cf.vector_plane_wave(
-        field, k, E0, power=2.0, pupil=partial(cf.square_pupil, w=10.0)
+        field, k, E0, power=1.0, pupil=partial(cf.square_pupil, w=10.0)
     )
 
-    field = cf.linear_polarizer(field, 1 / 2 * jnp.pi)
+    field = cf.linear_polarizer(field, angle=angle)
+
+    # check shape
     assert_axis_dimension(field.u, -1, 3)
-    # TODO: add another test
+
+    # check power - malus law
+    assert jnp.allclose(field.power.squeeze(), power)
 
 
 def test_left_circular_polarizer():
