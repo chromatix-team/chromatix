6a07661b9c1a4204c78bd65d4c6fd55b4e14fa5b
Diptodip <dd@d2d.sh>
2023-04-09 15:35:09 -0400

    Improve explanation of scalar/vector in docs

--- src/chromatix/field.py
+++ src/chromatix/field.py
@@ -16,52 +16,48 @@ class Field(struct.PyTreeNode):
     """
     A container that describes the chromatic light field at a 2D plane.
 
-    ``Field`` objects track various attributes of a complex-valued scalar
-    field (in addition to the field itself for each wavelength): the spacing
-    of the samples along the field, the wavelengths in the spectrum, and the
-    density of the wavelengths. This information can be used, for example, to
-    calculate the intensity of a field at a plane, appropriately weighted by
-    the spectrum. ``Field`` objects also provide various grids for convenience,
-    as well as allow elementwise operations with any broadcastable values,
-    including scalars, arrays, or other ``Field`` objects. These operations
-    include: `+`, `-` (including negation), `*`, `/`, `+=`, `-=`, `*=`, `/=`.
+    ``Field`` objects track various attributes of a complex-valued field (in
+    addition to the field itself for each wavelength): the spacing of the
+    samples along the field, the wavelengths in the spectrum, and the density
+    of the wavelengths. This information can be used, for example, to calculate
+    the intensity of a field at a plane, appropriately weighted by the spectrum.
+    ``Field`` objects also provide various grids for convenience, as well
+    as allow elementwise operations with any broadcastable values, including
+    scalars, arrays, or other ``Field`` objects. These operations include: `+`,
+    `-` (including negation), `*`, `/`, `+=`, `-=`, `*=`, `/=`.
 
     The shape of a ``Field`` object is `(B... H W C [1 | 3])`, where B... is
-    an arbitrary number of batch dimensions, H and W are height and width, and
-    C is the channel dimension, which we use for different wavelengths in the
-    spectrum of a ``Field``. The (potentially more than 1) batch dimensions
-    can be used for any purpose, such as different samples, depth, or time. Any
-    propagations using Chromatix elements or functions that produce multiple
-    depths will broadcast to the innermost batch dimension. If more dimensions
-    are required, we encourage the use of ``jax.vmap``, ``jax.pmap``, or a
-    combination of the two. We intend for this to be a compromise between not
-    having too many dimensions when they are not required, and also not having
-    to litter a program with ``jax.vmap`` transformations for common simulations
-    in 3D or 3D over time. The very last dimension is either 1 for fields in the
-    scalar approximation or 3 for vectorial fields, representing the field in
-    all three directions.
+    an arbitrary number of batch dimensions, H and W are height and width,
+    and C is the channel dimension, which we use for different wavelengths in
+    the spectrum of a ``Field``. The final dimension has size either 1 for a
+    scalar approximation ``ScalarField`` or 3 for the full vectorial case of
+    ``VectorField``. Any function in Chromatix that deals with ``Field``s can
+    work with either ``ScalarField``s or ``VectorField``s, unless otherwise
+    stated.
 
-    Concretely, this means simulations that are only in 2D and with a single
-    wavelength will always have at least two distinct singleton dimensions
-    (three in the scalar approximation because the final dimension will have
-    size 1). Any simulations taking into account either depth or multiple
-    wavelengths will not need to use JAX transformations or loops. Any
-    simulations requiring both 3D spatial dimensions and the tracking of the
-    field across time will either have to loop through time or use ``jax.vmap``/
-    ``jax.pmap``.
+    The (potentially more than 1) batch dimensions can be used for any purpose,
+    such as different samples, depth, or time. Any Chromatix functions that
+    produce multiple depths (e.g. propagation to multiple z values) will
+    broadcast to the innermost batch dimension. If more dimensions are required,
+    we encourage the use of ``jax.vmap``, ``jax.pmap``, or a combination of
+    the two. We intend for this to be a compromise between not having too many
+    dimensions when they are not required, and also not having to litter a
+    program with ``jax.vmap`` transformations for common simulations in 3D or 3D
+    over time.
 
     Due to this shape, in order to ensure that attributes of ``Field``
-    objects broadcast appropriately, attributes which could be 1D arrays
-    are ensured to have extra singleton dimensions. In order to make the
-    creation of a ``Field`` object more convenient, we provide the class
-    method ``Field.create()`` (detailed below), which accepts scalar or 1D
-    array arguments for the various attributes (e.g. if a single wavelength
-    is desired, a scalar value can be used, but if multiple wavelengths are
-    desired, a 1D array can be used for the value of ``spectrum``). This method
-    appropriately reshapes the attributes provided to the correct shapes.
+    objects broadcast appropriately, attributes which could be 1D arrays are
+    ensured to have extra singleton dimensions. In order to make the creation
+    of ``Field`` objects more convenient, we provide the class methods
+    ``ScalarField.create()`` and ``VectorField.create()`` (detailed below),
+    which accepts scalar or 1D array arguments for the various attributes
+    (e.g. if a single wavelength is desired, a scalar value can be used, but if
+    multiple wavelengths are desired, a 1D array can be used for the value of
+    ``spectrum``). These methods appropriately reshapes the attributes provided
+    to the correct shapes.
 
     Attributes:
-        u: The scalar field of shape `(B... H W C [1 | 3])`.
+        u: The complex field of shape `(B... H W C [1 | 3])`.
         dx: The spacing of the samples in ``u`` discretizing a continuous
             field. Can either be a 1D array with the same size as the number
             of wavelengths in the spectrum of shape (C), specifying a square
@@ -121,24 +117,24 @@ class Field(struct.PyTreeNode):
 
     @property
     def phase(self) -> Array:
-        """Phase of the complex scalar field, shape `(B... H W C [1 | 3])`."""
+        """Phase of the complex field, shape `(B... H W C [1 | 3])`."""
         return jnp.angle(self.u)
 
     @property
     def amplitude(self) -> Array:
-        """Amplitude of the complex scalar field, shape `(B... H W C [1 | 3])`."""
+        """Amplitude of the complex field, shape `(B... H W C [1 | 3])`."""
         return jnp.abs(self.u)
 
     @property
     def intensity(self) -> Array:
-        """Intensity of the complex scalar field, shape `(B... H W 1 1)`."""
+        """Intensity of the complex field, shape `(B... H W 1 1)`."""
         return jnp.sum(
             self.spectral_density * jnp.abs(self.u) ** 2, axis=(-2, -1), keepdims=True
         )
 
     @property
     def power(self) -> jnp.ndarray:
-        """Power of the complex scalar field, shape `(B... 1 1 1)`."""
+        """Power of the complex field, shape `(B... 1 1 1)`."""
         area = jnp.prod(self.dx, axis=0, keepdims=False)
         return jnp.sum(self.intensity, axis=(-4, -3), keepdims=True) * area
 
@@ -280,7 +276,7 @@ class ScalarField(Field):
         assert (
             ndim >= 5
         ), "Field must be Array with at least 5 dimensions: (B... H W C 1)."
-        assert u.shape[-1] == 1, "Last dimension of Array must be 1 for scalar fields."
+        assert u.shape[-1] == 1, "Last dimension must be 1 for scalar fields."
         assert_equal_shape([spectrum, spectral_density])
         spectrum = _broadcast_1d_to_channels(spectrum, ndim)
         spectral_density = _broadcast_1d_to_channels(spectral_density, ndim)
@@ -326,7 +322,7 @@ class VectorField(Field):
                 Will be scaled to sum to 1.0 over all wavelengths. Should be a
                 1D array containing the weight of each wavelength, or a float
                 for a single wavelength.
-            u: The scalar field of shape `(B... H W C 3)`. If not given,
+            u: The vectorial field of shape `(B... H W C 3)`. If not given,
                 the ``Field`` is allocated with uninitialized values of the
                 given ``shape`` as `(1 H W C 3)`.
             shape: A tuple defining the shape of only the spatial
@@ -344,7 +340,7 @@ class VectorField(Field):
         assert (
             ndim >= 5
         ), "Field must be Array with at least 5 dimensions: (B... H W C 3)."
-        assert u.shape[-1] == 3, "Last dimension of Array must be 3 for vector fields."
+        assert u.shape[-1] == 3, "Last dimension must be 3 for vectorial fields."
         assert_equal_shape([spectrum, spectral_density])
         spectrum = _broadcast_1d_to_channels(spectrum, ndim)
         spectral_density = _broadcast_1d_to_channels(spectral_density, ndim)
@@ -359,5 +355,5 @@ class VectorField(Field):
     def jones_vector(self) -> Array:
         """Return Jones vector of field."""
         norm = jnp.linalg.norm(self.u, axis=-1, keepdims=True)
-        norm = jnp.where(norm == 0, 1, norm)  # set zeros to 1 because u = 0 anyway
+        norm = jnp.where(norm == 0, 1, norm)  # set to 1 to avoid division by zero
         return self.u / norm
