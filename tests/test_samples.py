9b36d7155236587fbcd827bc187350a93f34e113
Amey Chaware <amey.chaware@gmail.com>
2023-04-10 15:25:32 -0400

    modify tests for new field

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,15 +1,16 @@
 import jax.numpy as jnp
 import pytest
 from chromatix.functional.samples import multislice_thick_sample, thin_sample
-from chromatix.functional.sources import empty_field, plane_wave
+from chromatix.functional.sources import plane_wave
 
 
 def test_zero_thin_sample():
     # all zero sample, no effect on incoming field expected
-    absorption = jnp.zeros((1, 2, 2, 1))
-    dn = jnp.zeros((1, 2, 2, 1))
-    field = empty_field((2, 2), 0.1, 0.532, 1.0)
-    field = plane_wave(field, power=1)
+    field = plane_wave(
+        shape=(2, 2), dx=0.1, spectrum=0.532, spectral_density=1.0, power=1.0
+    )
+    absorption = jnp.zeros_like(field.u)
+    dn = jnp.zeros_like(field.u)
     out_field = thin_sample(field, absorption, dn, thickness=1)
 
     assert jnp.allclose(field.u, out_field.u)
@@ -17,10 +18,11 @@ def test_zero_thin_sample():
 
 def test_phase_delay_thin_sample():
     # pure phase sample, no power difference expected
-    absorption = jnp.zeros((1, 2, 2, 1))
-    dn = jnp.ones((1, 2, 2, 1)) * 0.5  # half cycle delay
-    field = empty_field((2, 2), 0.1, 0.532, 1.0)
-    field = plane_wave(field, power=1)
+    field = plane_wave(
+        shape=(2, 2), dx=0.1, spectrum=0.532, spectral_density=1.0, power=1.0
+    )
+    absorption = jnp.zeros_like(field.u)
+    dn = jnp.ones_like(field.u) * 0.5  # half cycle delay
     out_field = thin_sample(field, absorption, dn, thickness=0.532)
 
     assert jnp.allclose(field.power, out_field.power)
@@ -28,12 +30,13 @@ def test_phase_delay_thin_sample():
 
 
 def test_absorption_only_thin_sample():
-    # pure phase sample, no power difference expected
-    absorption = jnp.ones((1, 2, 2, 1)) / (2 * jnp.pi)  # attenuation factor of 1/e
-    dn = jnp.zeros((1, 2, 2, 1))
-    field = empty_field((2, 2), 0.1, 0.532, 1.0)
-    field = plane_wave(field, power=1)
+    # pure absorption sample, no phase difference expected
+    field = plane_wave(
+        shape=(2, 2), dx=0.1, spectrum=0.532, spectral_density=1.0, power=1.0
+    )
+    absorption = jnp.ones_like(field.u) / (2 * jnp.pi)  # attenuation factor of 1/e
+    dn = jnp.zeros_like(field.u)
     out_field = thin_sample(field, absorption, dn, thickness=0.532)
 
-    assert jnp.allclose(field.power, out_field.power*jnp.exp(2))
-    assert jnp.allclose(field.u, out_field.u*jnp.e)
+    assert jnp.allclose(field.power, out_field.power * jnp.exp(2))
+    assert jnp.allclose(field.u, out_field.u * jnp.e)
