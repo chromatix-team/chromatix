a44c7cd43936003556df8074597271b65d19042d
Diptodip <dd@d2d.sh>
2023-04-18 00:50:51 -0400

    Clean comments and imports

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,5 +1,4 @@
 import jax.numpy as jnp
-import pytest
 from chromatix.functional.samples import multislice_thick_sample, thin_sample
 from chromatix.functional.sources import plane_wave
 
@@ -12,7 +11,6 @@ def test_zero_thin_sample():
     absorption = jnp.zeros_like(field.u)
     dn = jnp.zeros_like(field.u)
     out_field = thin_sample(field, absorption, dn, thickness=1)
-
     assert jnp.allclose(field.u, out_field.u)
 
 
@@ -24,7 +22,6 @@ def test_phase_delay_thin_sample():
     absorption = jnp.zeros_like(field.u)
     dn = jnp.ones_like(field.u) * 0.5  # half cycle delay
     out_field = thin_sample(field, absorption, dn, thickness=0.532)
-
     assert jnp.allclose(field.power, out_field.power)
     assert jnp.allclose(field.u, -out_field.u)
 
@@ -39,7 +36,6 @@ def test_absorption_only_thin_sample():
     )  # gives attenuation factor of 1/e
     dn = jnp.zeros_like(field.u)
     out_field = thin_sample(field, absorption, dn, thickness=0.532)
-
     assert jnp.allclose(field.power, out_field.power * jnp.exp(2))
     assert jnp.allclose(field.u, out_field.u * jnp.e)
 
@@ -77,7 +73,6 @@ def test_absorption_only_thick_sample():
         thickness_per_slice=0.532,
         N_pad=0,
     )
-    # assert jnp.allclose(field.power, out_field.power * jnp.exp(2))
     assert jnp.allclose(field.u, out_field.u * jnp.exp(4))
 
 
@@ -96,6 +91,5 @@ def test_phase_delay_thick_sample():
         thickness_per_slice=0.532,
         N_pad=0,
     )
-
     assert jnp.allclose(field.power, out_field.power)
     assert jnp.allclose(field.u, out_field.u)
