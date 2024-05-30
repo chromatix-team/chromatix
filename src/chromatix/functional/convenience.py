dba3bf071f90314eb3b3efd72ea063bd0b193ce2
Diptodip Deb <dd@d2d.sh>
2024-05-30 13:19:58 -0400

    Merge conditional_fft and optical_fft
    Because we always want to support arbitrary combinations of positive and
    negative directions in optical_fft, @RainerHeintzmann implemented
    conditional_fft to support this. This commit just merges these functions
    into one because there is no reason to have two separate functions if
    this is always the desired behavior.

--- src/chromatix/functional/convenience.py
+++ src/chromatix/functional/convenience.py
@@ -2,64 +2,52 @@ import jax.numpy as jnp
 from chromatix.utils import _squeeze_grid_to_2d
 from chromatix.utils.fft import fft, ifft
 from chromatix import Field
+from typing import Union
 
 
-def conditional_fft(field: Field, z: float, n: float):
+def optical_fft(field: Field, z: Union[float, Array], n: float) -> Field:
     """
-    Computes the ``fft`` or th ``ifft`` on an incoming ``Field`` propagated by ``z``, depending on the sign of ``z``.
-    If ``z`` is positive an ``fft```will be performed, otherwise an ``ifft``. The reason is that Signle step Fresnel propagation
-    has a 1/(lambda z) term in the FFT, which therefore should yield this behaviour.
-    Note that the required norm factors are also included in this fft.
-    This fairly complicated way of calculating an ifft in terms of an fft is needed, since we want to enable a potential mix of positive and
-    negative values for ``z``.
-    """
-    L_sq = field.spectrum * z / n  # Lengthscale L^2
-    du = field.dk * jnp.abs(L_sq)  # New spacing. pixel spacing is always positive
-    shifter = jnp.exp(
-        1j * (1 * field.k_grid[0] + 1 * field.k_grid[1])
-    )  # shifts by one pixel in x and y (all sizes are even!)
-    norm_fft = (  # fft for positive z
-        (z >= 0) * -1j * jnp.prod(field.dx, axis=0, keepdims=False) / L_sq
-    )
-    norm_ifft = (  # ifft for negative z, needs to use the conjugate and pre-apply a one pixel shift
-        -1j  # also account for the conjugate beeing applied below
-        * (z < 0)
-        * (  # since all the steps should be run in backwards order we have to devide. However z is negative which changes the sign of norm, requiring the phase change
-            L_sq / jnp.prod(du, axis=0, keepdims=False)
-        )
-        / jnp.prod(
-            jnp.array(field.shape)
-        )  # due to a different norm factor for fft and ifft
-    )
-    fft_input = norm_fft * field.u + norm_ifft * field.conj.u * shifter
-    fft_output = fft(fft_input, axes=field.spatial_dims, shift=True)
-    return (z >= 0) * fft_output + (z < 0) * jnp.conj(fft_output)
-
-
-def optical_fft(field: Field, z: float, n: float) -> Field:
-    """
-    Computes the optical ``fft`` on an incoming ``Field`` propagated by ``z``.
-    This means that this function appropriately changes the sampling of the
-    ``Field`` that is output (after propagating to some distance ``z``), and
-    also computes the correct ``fftshift``/``ifftshift`` as needed.
-    Optionally, this function can also compute the ``ifft`` instead (which is
-    useful to prevent outputs from being flipped if that is not desired).
+    Computes the optical ``fft`` or ``ifft`` on an incoming ``Field`` propagated
+    by ``z``, depending on the sign of ``z`` (which may be an array containing
+    both positive and negative values). If ``z`` is positive an ``fft```will
+    be performed, otherwise an ``ifft`` (due to the ``1 / (lambda * z)`` term
+    in the single Fourier transform Fresnel propagation, which requires this
+    behavior). The ``ifft`` is calculated in terms of the conjugate of the
+    ``fft`` with appropriate normalization applied so that propagating forwards
+    and then backwards yields the same ``Field`` up to numerical precision. This
+    function also appropriately changes the sampling of the ``Field`` that is
+    output (after propagating to some distance(s) ``z``).
     Args:
         field: The ``Field`` to be propagated by ``fft``.
-        z: The distance the ``Field`` will be propagated.
-        n: Refractive index.
+        z: The distance the ``Field`` will be propagated, assumed to be
+            broadcasted to the innermost batch dimension.
+        n: Refractive index of the propagation medium.
     Returns:
         The propagated ``Field``, transformed by ``fft``/``ifft``.
     """
-    L_sq = field.spectrum * z / n  # Lengthscale L^2
-    du = field.dk * jnp.abs(L_sq)  # New spacing is always positive
-    u = conditional_fft(field, z, n)
-    # if z < 0:
-    #   norm = jnp.prod(du, axis=0, keepdims=False) / L_sq
-    #   u = ifft(
-    #     field.u * (1j / norm), axes=field.spatial_dims, shift=True
-    #   )  # since all the steps should be run in backwards order we have to devide. However z is negative which changes the sign of norm, requiring the phase change
-    # else:
-    #   norm = jnp.prod(field.dx, axis=0, keepdims=False) / L_sq
-    #   u = -1j * norm * fft(field.u, axes=field.spatial_dims, shift=True)
+    L_sq = field.spectrum * z / n
+    du = field.dk * jnp.abs(L_sq)
+    # Shift by one pixel in x and y when z < 0 and we ifft
+    shifter = jnp.exp(
+        1j * (1 * field.k_grid[0] + 1 * field.k_grid[1])
+    )
+    # Forward transform normalization for z >= 0
+    norm_fft = (
+        (z >= 0) * -1j * jnp.prod(field.dx, axis=0, keepdims=False) / L_sq
+    )
+    # Inverse transform normalization for z < 0
+    norm_ifft = (
+        (z < 0)
+        * -1j  # Sign change because we take the conjugate of the input
+        * (
+            L_sq / jnp.prod(du, axis=0, keepdims=False)  # Inverse length scale
+        )
+        / jnp.prod(
+            jnp.array(field.shape)  # Due to a different norm factor for fft and ifft
+        )
+    )
+    # Inverse transform input needs to use the conjugate and pre-apply a one pixel shift
+    fft_input = (norm_fft * field.u) + (norm_ifft * field.conj.u * shifter)
+    fft_output = fft(fft_input, axes=field.spatial_dims, shift=True)
+    u = (z >= 0) * fft_output + (z < 0) * jnp.conj(fft_output)
     return field.replace(u=u, _dx=_squeeze_grid_to_2d(du, field.ndim))
