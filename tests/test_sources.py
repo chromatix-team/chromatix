0b5cdce265660d402128dd589090fddacd336009
Gert-Jan Both <bothg@hhmi.org>
2024-06-11 11:54:33 -0400

    Add more tests.

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -8,56 +8,76 @@ import pytest
 from chex import assert_shape
 
 
-@pytest.mark.parametrize(
-    "power, shape, pupil",
-    [
-        (1.0, (512, 512), partial(cf.circular_pupil, w=10.0)),
-        (100.0, (256, 256), None),
-    ],
-)
-def test_plane_wave(power, shape, pupil):
-    field = cf.plane_wave(shape, 0.1, 0.532, 1.0, power=power, pupil=pupil)
-
-    assert jnp.allclose(field.power, power)
-    assert_shape(field.u, (1, *shape, 1, 1))
-
-
-@pytest.mark.parametrize(
-    "power, amplitude, shape, pupil",
-    [
-        (1.0, cf.linear(jnp.pi / 2), (512, 512), partial(cf.circular_pupil, w=10.0)),
-        (100.0, cf.left_circular(), (256, 256), None),
-    ],
-)
-def test_plane_wave_vectorial(power, amplitude, shape, pupil):
-    field = cf.plane_wave(
-        shape,
-        0.1,
-        0.532,
-        1.0,
-        power=power,
-        amplitude=amplitude,
-        pupil=pupil,
-        scalar=False,
+class TestPlaneWave:
+    @pytest.mark.parametrize(
+        "power, shape, pupil",
+        [
+            (1.0, (512, 512), partial(cf.circular_pupil, w=10.0)),
+            (100.0, (256, 256), None),
+        ],
     )
+    def test_scalar(self, power, shape, pupil):
+        field = cf.plane_wave(shape, 0.1, 0.532, 1.0, power=power, pupil=pupil)
 
-    assert jnp.allclose(field.power, power)
-    assert_shape(field.u, (1, *shape, 1, 3))
+        assert jnp.allclose(field.power, power)
+        assert_shape(field.u, (1, *shape, 1, 1))
+
+    @pytest.mark.parametrize(
+        "power, amplitude, shape, pupil",
+        [
+            (
+                1.0,
+                cf.linear(jnp.pi / 2),
+                (512, 512),
+                partial(cf.circular_pupil, w=10.0),
+            ),
+            (100.0, cf.left_circular(), (256, 256), None),
+        ],
+    )
+    def test_vectorial(self, power, amplitude, shape, pupil):
+        field = cf.plane_wave(
+            shape,
+            0.1,
+            0.532,
+            1.0,
+            power=power,
+            amplitude=amplitude,
+            pupil=pupil,
+            scalar=False,
+        )
+
+        assert jnp.allclose(field.power, power)
+        assert_shape(field.u, (1, *shape, 1, 3))
+
+    @pytest.mark.parametrize(
+        "amplitude, scalar",
+        [
+            (jnp.ones((2,)), True),
+            (jnp.ones((2,)), False),
+            (jnp.ones((1,)), False),
+            (jnp.ones((3,)), True),
+        ],
+    )
+    def test_wrong_amplitude_size(self, amplitude: Array, scalar: bool):
+        # Using wrong shape for amplitude should raise Assertion error
+        with pytest.raises(AssertionError):
+            _ = cf.plane_wave(
+                (256, 256),
+                0.1,
+                0.532,
+                1.0,
+                amplitude=amplitude,
+                scalar=scalar,
+                power=1.0,
+            )
 
 
 class TestObjectivePointSource:
     @pytest.mark.parametrize(
-        "power, z, shape, pupil",
-        [
-            (1.0, 0.5, (512, 512), partial(cf.square_pupil, w=10.0)),
-            (100.0, 1.0, (256, 256), None),
-            (20.0, 2, (256, 512), None),  # should work for z =0
-        ],
+        "power, z, shape",
+        [(1.0, 0.5, (512, 512)), (100.0, 1.0, (256, 256)), (50, 0.0, (256, 512))],
     )
-    @pytest.mark.parametrize(
-        "power, z, shape", [(1.0, 0.5, (512, 512)), (100.0, 1.0, (256, 256))]
-    )
-    def test_scalar(power, z, shape):
+    def test_scalar(self, power, z, shape):
         field = cf.objective_point_source(
             shape, 0.1, 0.532, 1.0, z, 100.0, 1.33, NA=0.8, power=power
         )
@@ -93,17 +113,18 @@ class TestObjectivePointSource:
     def test_wrong_amplitude_size(self, amplitude: Array, scalar: bool):
         # Using wrong shape for amplitude should raise Assertion error
         with pytest.raises(AssertionError):
-            _ = cf.point_source(
+            _ = cf.objective_point_source(
                 (256, 256),
                 0.1,
                 0.532,
                 1.0,
                 1.0,
+                100,
+                1.0,
                 1.33,
                 amplitude=amplitude,
                 scalar=scalar,
                 power=1.0,
-                pupil=None,
             )
 
 
