7368b2fb95a6c848b55970de817e9a750e91a719
Diptodip <dd@d2d.sh>
2024-09-13 16:26:15 -0400

    Add rectangular pupil function

--- src/chromatix/functional/pupils.py
+++ src/chromatix/functional/pupils.py
@@ -4,7 +4,7 @@ from chromatix.field import Field
 from chromatix.typing import ScalarLike
 from chromatix.utils import l2_norm, l2_sq_norm, linf_norm
 
-__all__ = ["circular_pupil", "square_pupil", "super_gaussian_pupil", "tukey_pupil"]
+__all__ = ["circular_pupil", "square_pupil", "rectangular_pupil", "super_gaussian_pupil", "tukey_pupil"]
 
 
 def circular_pupil(field: Field, w: ScalarLike) -> Field:
@@ -19,6 +19,15 @@ def circular_pupil(field: Field, w: ScalarLike) -> Field:
 def square_pupil(field: Field, w: ScalarLike) -> Field:
     """Applies square pupil with side length ``w`` to ``field``."""
     mask = linf_norm(field.grid) <= (field.spectrum[..., 0, 0] / field.spectrum) * w / 2
+    return field * mask
+
+
+def rectangular_pupil(field: Field, h: ScalarLike, w: ScalarLike) -> Field:
+    """Applies rectangular pupil with height ``h`` and width ``w`` to ``field``."""
+    distance = jnp.abs(field.grid)
+    mask_h = distance[0] <= (field.spectrum[..., 0, 0] / field.spectrum) * h / 2
+    mask_w = distance[1] <= (field.spectrum[..., 0, 0] / field.spectrum) * w / 2
+    mask = mask_h & mask_w
     return field * mask
 
 
