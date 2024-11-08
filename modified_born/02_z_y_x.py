1bf0ed4dfe7f573a42b0617dbd316f674525a462
Gert-Jan Both <bothg@hhmi.org>
2024-11-08 09:53:39 -0500

    Fixed small shape issue.

--- modified_born/02_z_y_x.py
+++ modified_born/02_z_y_x.py
@@ -25,28 +25,38 @@ plt.xlabel("z")
 plt.ylabel("x")
 plt.colorbar(label="Refractive index")
 
+
 # %% Now adding the absorbing boundary conditions
 # Finding new shapes and rois
-def add_bc(permittivity: Array, width: tuple[float | None, ...], spacing: float, wavelength: float, strength: float | None = None, alpha: float | None = None, order: int = 4) -> tuple[Array, tuple[slice, ...]]:
+def add_bc(
+    permittivity: Array,
+    width: tuple[float | None, ...],
+    spacing: float,
+    wavelength: float,
+    strength: float | None = None,
+    alpha: float | None = None,
+    order: int = 4,
+) -> tuple[Array, tuple[slice, ...]]:
     """
     width is in wavelengths. Use None for periodic BCs.
     """
-    
-    
+
     # Figuring out new size
     n_pad = tuple(0 if width_i is None else int(width_i / spacing) for width_i in width)
     roi = tuple(slice(n, n + size) for n, size in zip(n_pad, permittivity.shape))
 
     # Padding permittivity to new size
-    # We repeat the mean value 
+    # We repeat the mean value
     padding = [(0, 0) for _ in range(permittivity.ndim)]
     for idx, n in enumerate(n_pad):
         padding[idx] = (n, n)
-    permittivity = jnp.pad(permittivity, padding, mode="constant", constant_values = jnp.mean(permittivity))
+    permittivity = jnp.pad(
+        permittivity, padding, mode="constant", constant_values=jnp.mean(permittivity)
+    )
 
     # Gathering constants
     k0 = 2 * jnp.pi / wavelength
-    km = k0 * jnp.sqrt(jnp.mean(permittivity)) 
+    km = k0 * jnp.sqrt(jnp.mean(permittivity))
     match (strength, alpha):
         case (Number(), None):
             alpha = strength * km / 2
@@ -73,21 +83,24 @@ def add_bc(permittivity: Array, width: tuple[float | None, ...], spacing: float,
 
     numerator = alpha**2 * (order - ar + 2 * 1j * km * r) * ar ** (order - 1)
     denominator = P * factorial(order, exact=True)
-    boundary =  1 / k0**2 * numerator / denominator
-    
+    boundary = 1 / k0**2 * numerator / denominator
+
     # Inside the ROI it's 0
     boundary = boundary.at[roi].set(0)
 
     return permittivity + boundary, roi
 
+
 # %%
 spacing = 0.1
-wavelength = 1.0 
+wavelength = 1.0
 width = (25 / wavelength, None, 25 / wavelength)
-alpha_boundary = 0.35 
+alpha_boundary = 0.35
 order = 4
 
-permittivity, roi = add_bc(n_sample**2, width, spacing, wavelength, alpha=alpha_boundary, order=order)
+permittivity, roi = add_bc(
+    n_sample**2, width, spacing, wavelength, alpha=alpha_boundary, order=order
+)
 print(f"Padded permittivity shape: {permittivity.shape}")
 
 # %%
@@ -114,8 +127,12 @@ plt.legend()
 # We know that the real part of the permittivity needs to go to km^2 - alpha*2, although we're far form infinity
 # and the imaginary part at 2*km*alpha from th original born series paper.
 k0 = 2 * jnp.pi / wavelength
-print(f"Real permittivity should saturate at {-alpha_boundary**2:.2f}, observed {k0**2 * (permittivity[0, 0, 0].real - jnp.mean(permittivity.real[roi])):.2f}")
-print(f"Imaginary permittivity should saturate at {2 * alpha_boundary * 2 * jnp.pi * jnp.sqrt(jnp.mean(permittivity[roi].real) / wavelength):.2f}, observed {k0**2 * (permittivity[0, 0, 0].imag - jnp.mean(permittivity.imag[roi])):.2f}")
+print(
+    f"Real permittivity should saturate at {-alpha_boundary**2:.2f}, observed {k0**2 * (permittivity[0, 0, 0].real - jnp.mean(permittivity.real[roi])):.2f}"
+)
+print(
+    f"Imaginary permittivity should saturate at {2 * alpha_boundary * 2 * jnp.pi * jnp.sqrt(jnp.mean(permittivity[roi].real) / wavelength):.2f}, observed {k0**2 * (permittivity[0, 0, 0].imag - jnp.mean(permittivity.imag[roi])):.2f}"
+)
 
 # %% Now we can start solving
 # The optimal alpha is given by:
@@ -126,6 +143,8 @@ alpha = alpha_real + 1j * alpha_imag
 
 print(f"Optimal background wavenumber: {alpha:.2f}")
 print(f"Expected propagation speed [wavelength^-1]: {2 * alpha_real / alpha_imag :2f}")
+
+
 # %% Padding shapes
 # Because we have absorbing BCs, we don't need to double the size to prevent circular convolution, saving a lot of time!
 # We just pad to the next power of 2:
@@ -137,7 +156,8 @@ def pad_fourier(x: Array) -> Array:
         return new_size, (0, new_size - size)
 
     return zip(*[n_pad(shape) for shape in x.shape])
-  
+
+
 padded_shape, padding = pad_fourier(permittivity)
 print(f"Padded shape will be {padded_shape}")
 
@@ -150,11 +170,13 @@ k_grid = jnp.stack(jnp.meshgrid(*ks, indexing="ij"), axis=-1)
 assert jnp.diff(k_grid[:, 0, 0, 0])[0] != 0.0
 assert jnp.diff(k_grid[0, 0, :, 2])[0] != 0.0
 
+
 # %% Making the Greens function
 def G_fn(k: Array, k0: ArrayLike, alpha: Array) -> Array:
-    k_sq = jnp.sum(k ** 2, axis=-1)[..., None, None]
+    k_sq = jnp.sum(k**2, axis=-1)[..., None, None]
     k_cross = k[..., :, None] * k[..., None, :] / (alpha * k0**2)
     return (jnp.eye(3) - k_cross) / (k_sq - alpha * k0**2)
+
 
 G = G_fn(k_grid, k0, alpha)
 print(f"Shape of Greens function {G.shape}")
@@ -176,9 +198,9 @@ plt.imshow(jnp.fft.fftshift(G[:, 0, :, 0, 2].real))
 plt.colorbar(fraction=0.046, pad=0.04)
 
 # %% Making the potential
-V = (permittivity - alpha)
+V = permittivity - alpha
 
-# %% Now making a source. To prevent aliasing, we settle on a 2D tukey 
+# %% Now making a source. To prevent aliasing, we settle on a 2D tukey
 source = jnp.zeros((*permittivity.shape, 3))
 source = source.at[250, :, :].set(k0**2 * jnp.array([0, 1, 1]))
 
@@ -193,42 +215,58 @@ plt.title("Ey of source")
 plt.imshow(source[:, 0, :, 1])
 plt.colorbar(fraction=0.046, pad=0.04)
 
+
 # %% These function we use solve the problem
 def bmatvec(mat: Array, vec: Array) -> Array:
     return jnp.matmul(mat, vec[..., None]).squeeze(-1)
 
+
 def pad(field: Array) -> Array:
     return jnp.pad(field, (*padding, (0, 0)))
 
+
 def crop(field: Array) -> Array:
-    return field[:V.shape[0], :V.shape[1], :V.shape[2], :]
+    return field[: V.shape[0], : V.shape[1], : V.shape[2], :]
+
 
 fft = lambda x: jnp.fft.fftn(x, axes=(0, 1, 2))
 ifft = lambda x: jnp.fft.ifftn(x, axes=(0, 1, 2))
 
+
 def propagate(G: Array, field: Array) -> Array:
     return crop(ifft(bmatvec(G, fft(pad(field)))))
-   
+
+
 def update_fn(args):
     field, history, iteration = args
 
     # New field
-    dE = 1j / alpha_imag * V * (propagate(G, k0**2 * V * field + source) - field)
+    dE = (
+        1j
+        / alpha_imag
+        * V[..., None]
+        * (propagate(G, k0**2 * V[..., None] * field + source) - field)
+    )
 
     # Calculating change
     delta = jnp.mean(jnp.abs(dE) ** 2) / jnp.mean(jnp.abs(field) ** 2)
 
     return field + dE, history.at[iteration].set(delta), iteration + 1
 
+
 def cond_fn(args) -> bool:
     _, history, iteration = args
     return (history[iteration - 1] > rtol) & (iteration < max_iter)
+
+
 # %%
 rtol = 1e-6
 max_iter = 1000
 
 init = update_fn((jnp.zeros_like(source), jnp.zeros(max_iter), 0))
-field, history, iteration = jax.block_until_ready(jax.lax.while_loop(cond_fn, update_fn, init))
+field, history, iteration = jax.block_until_ready(
+    jax.lax.while_loop(cond_fn, update_fn, init)
+)
 history = history[:iteration]
 # %%
 plt.title("Relative change in field")
@@ -251,10 +289,10 @@ plt.title("Ez")
 plt.imshow(jnp.rot90(jnp.abs(field[roi][:, 0, :, 0])))
 
 # %% As a check - is there any energy in the padding area?
-total_power = jnp.sum(jnp.abs(field)**2)
-no_padding_power = jnp.sum(jnp.abs(field[:1500, :, :1500])**2)
-power_in_padding = total_power - no_padding_power 
-power_in_BC = no_padding_power - jnp.sum(jnp.abs(field[roi]**2))
+total_power = jnp.sum(jnp.abs(field) ** 2)
+no_padding_power = jnp.sum(jnp.abs(field[:1500, :, :1500]) ** 2)
+power_in_padding = total_power - no_padding_power
+power_in_BC = no_padding_power - jnp.sum(jnp.abs(field[roi] ** 2))
 
 print(f"Fraction of power in padding: {power_in_padding / total_power:.2f}")
 print(f"Fraction of power in BC: {power_in_BC / total_power:.2f}")
