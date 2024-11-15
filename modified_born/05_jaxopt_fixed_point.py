ccbddb633aee1a1ffeb760efdb95fdc7fb980fad
Gert-Jan Both <gertjan.both@protonmail.com>
2024-11-15 15:52:55 -0500

    Simplify the fixed point solver.

--- modified_born/05_jaxopt_fixed_point.py
+++ modified_born/05_jaxopt_fixed_point.py
@@ -23,10 +23,11 @@ field = cf.plane_wave(
     wavelength,
     amplitude=jnp.array([0, 1, 1]),
 )
-# 250 voxels = 25 wavelengths = 25 mum
-field, results = jit(
-    thick_sample_exact, static_argnames=("boundary_width", "max_steps")
-)(field, sample, (250, None, 250))
+print(sample.shape)
+# %% 250 voxels = 25 wavelengths = 25 mum
+forward = jit(thick_sample_exact, static_argnames=("boundary_width", "max_steps"))
+# At least 250; 1536 - 1000 = 536 -> best padding; 536 / 2
+field, results = forward(field, sample, (268, None, 268))
 
 print(f"Converged in {results.n_steps} iterations.")
 print(f"Relative error of {results.rel_error}")
@@ -35,21 +36,21 @@ plt.figure(figsize=(15, 5))
 plt.subplot(131)
 plt.title("Ex")
 plt.imshow(
-    jnp.rot90(jnp.abs(results.field[sample.roi][:, 0, :, 2])), vmin=0.0, vmax=1.2
+    jnp.rot90(jnp.abs(results.field[results.roi][:, 0, :, 2])), vmin=0.0, vmax=1.2
 )
 plt.colorbar(fraction=0.046, pad=0.04)
 
 plt.subplot(132)
 plt.title("Ey")
 plt.imshow(
-    jnp.rot90(jnp.abs(results.field[sample.roi][:, 0, :, 1])), vmin=0.0, vmax=1.2
+    jnp.rot90(jnp.abs(results.field[results.roi][:, 0, :, 1])), vmin=0.0, vmax=1.2
 )
 plt.colorbar(fraction=0.046, pad=0.04)
 
 plt.subplot(133)
 plt.title("Ez")
 plt.imshow(
-    jnp.rot90(jnp.abs(results.field[sample.roi][:, 0, :, 0])), vmin=0.0, vmax=1.2
+    jnp.rot90(jnp.abs(results.field[results.roi][:, 0, :, 0])), vmin=0.0, vmax=1.2
 )
 plt.colorbar(fraction=0.046, pad=0.04)
 
--- modified_born/solvers.py
+++ modified_born/solvers.py
@@ -156,9 +156,10 @@ class Results(struct.PyTreeNode):
     field: Array
     rel_error: Array
     n_steps: int
+    roi: tuple[slice, ...] = struct.field(pytree_node=False)
 
 
-def maxwell_solver(
+def maxwell_solver_optimistix(
     sample: Sample,
     source: Source,
     rtol: float = 1e-3,
@@ -167,6 +168,8 @@ def maxwell_solver(
     field_init: Array | None = None,
     pad_fourier: bool = True,
 ) -> Results:
+    # NOTE: Optimistix implicit diff DOES NOT work right now, for some reason.
+    # WIll have to dig in later.
     # Physical methods
     def G_fn(k: Array, k0: ArrayLike, alpha: Array) -> Array:
         k_sq = jnp.sum(k**2, axis=-1)[..., None, None]
@@ -251,15 +254,12 @@ def maxwell_solver(
     return Results(field, results.state.relative_error, results.stats["num_steps"])
 
 
-def maxwell_solver_jaxopt(
+def maxwell_solver(
     sample: Sample,
     source: Source,
-    rtol: float = 1e-3,
-    atol: float = 1e-3,
+    tol: float = 1e-3,
     max_steps: int = 500,
     field_init: Array | None = None,
-    pad_fourier: bool = True,
-    implicit: bool = True,
 ) -> Results:
     # Physical methods
     def G_fn(k: Array, k0: ArrayLike, alpha: Array) -> Array:
@@ -273,7 +273,7 @@ def maxwell_solver_jaxopt(
         return ifft(bmatvec(G, fft(field)))
 
     # Iteration methods
-    def update_fn(u: Array, state) -> Array:
+    def update_fn(u: Array, state: tuple[Array, Array, Array, Array]) -> Array:
         V, source, G, alpha = state
         field = u[0] + 1j * u[1]  # making the complex field
         scattered_field = k0**2 * V * field + source
@@ -283,63 +283,31 @@ def maxwell_solver_jaxopt(
     # Calculating background wavenumber and potential
     # We DO NOT want the gradient of alpha, we just want the gradient to flow through V
     alpha_real = (sample.permittivity.real.min() + sample.permittivity.real.max()) / 2
-    alpha_imag = jnp.max(jnp.abs(sample.permittivity - alpha_real)) / 0.95
+    alpha_imag = jnp.max(jnp.abs(sample.permittivity - alpha_real)) / 0.99
     alpha = stop_gradient(alpha_real + 1j * alpha_imag)
-    _V = sample.permittivity[..., None] - alpha
-
-    # Padding source and potential to next power of 2
-    # We add 0 at the end to account that for source
-    if pad_fourier:
-        fourier_size = lambda size: int(2 ** (np.ceil(np.log2(size))))  # noqa: E731
-        padding = [fourier_size(size) - size if size > 1 else 0 for size in _V.shape]
-        padding = tuple((0, size) for size in padding)
-        padded_roi = tuple(
-            slice(int(n_pad // 2), int(size + n_pad // 2))
-            for size, (_, n_pad) in zip(sample.shape, padding)
-        )
-        sample_roi = tuple(
-            slice(roi_outer.start + roi_inner.start, roi_outer.start + roi_inner.stop)
-            for roi_outer, roi_inner in zip(padded_roi, sample.roi)
-        )
-
-        pad = lambda x: jnp.pad(x, padding, mode="empty")  # noqa: E731
-        _V, _source = pad(_V), pad(source.source)
-
-    else:
-        _source = source.source
-        sample_roi = sample.roi
+    V = sample.permittivity[..., None] - alpha
 
     # Making the Greens function
     k0 = 2 * jnp.pi / source.wavelength
-    ks = [2 * jnp.pi * jnp.fft.fftfreq(shape, sample.spacing) for shape in _V.shape[:3]]
+    ks = [2 * jnp.pi * jnp.fft.fftfreq(shape, sample.spacing) for shape in V.shape[:3]]
     k_grid = jnp.stack(jnp.meshgrid(*ks, indexing="ij"), axis=-1)
     G = G_fn(k_grid, k0, alpha)
 
     # Figuring out initial field
     if field_init is None:
-        field_init = jnp.zeros((2, *_source.shape))
+        field_init = jnp.zeros((2, *source.shape))
     else:
         field_init = jnp.stack([field_init.real, field_init.imag])
-        field_init = pad(field_init) if pad_fourier else field_init
-
-    # Custom norm for sample ROI
-    def norm_fun(x):
-        return jnp.max(jnp.abs(x[:, sample_roi[0], sample_roi[1], sample_roi[2]]))
 
     # JAXopt solver setup and execution
-    solver = jop.FixedPointIteration(
-        fixed_point_fun=update_fn, maxiter=max_steps, tol=rtol, implicit_diff=implicit
-    )
-
-    state = (_V, _source, G, alpha)
-    params, state = solver.run(field_init, state)
+    solver = jop.FixedPointIteration(update_fn, maxiter=max_steps, tol=tol)
+    params, state = solver.run(field_init, (V, source.source, G, alpha))
 
     # Convert result back to complex field
     field = params[0] + 1j * params[1]
-    if pad_fourier:
-        field = field[padded_roi]
-
-    return Results(field=field, rel_error=state.error, n_steps=state.iter_num)
+    return Results(
+        field=field, rel_error=state.error, n_steps=state.iter_num, roi=sample.roi
+    )
 
 
 def thick_sample_exact(
@@ -348,27 +316,17 @@ def thick_sample_exact(
     boundary_width: tuple[int | None],
     alpha: float = 0.35,
     order: int = 4,
-    rtol: float = 1e-3,
-    atol: float = 1e-3,
+    tol: float = 1e-3,
     max_steps: int = 500,
     sinc_width: float | None = None,
     field_init: Array | None = None,
-    implicit: bool = True,
-    pad_fourier: bool = True,
 ) -> tuple[VectorField, Results]:
     sample = add_absorbing_bc(
         sample, field.spectrum.squeeze(), boundary_width, alpha=alpha, order=order
     )
     source = plane_wave_source(field, sample, width=sinc_width)
-    results = maxwell_solver_jaxopt(
-        sample,
-        source,
-        rtol=rtol,
-        atol=atol,
-        max_steps=max_steps,
-        field_init=field_init,
-        pad_fourier=pad_fourier,
-        implicit=implicit,
+    results = maxwell_solver(
+        sample, source, tol=tol, max_steps=max_steps, field_init=field_init
     )
     field = field.replace(u=results.field[sample.roi][-1][None, ..., None, :])
     return field, results
