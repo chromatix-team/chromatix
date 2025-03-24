correct the grid scaling and the gaussian envelope formula

src/chromatix/elements/sources.py
@@ -121,9 +121,7 @@ class GaussianSource(nn.Module):
     n: Union[float, Callable[[PRNGKey], float]]
     NA: Union[float, Callable[[PRNGKey], float]]
     power: Union[float, Callable[[PRNGKey], float]] = 1.0
-    amplitude: Union[float, Array, Callable[[PRNGKey], Array]] = np.array(
-        [0.0, 0.0, 1.0]
-    )
+    amplitude: Union[float, Array, Callable[[PRNGKey], Array]] = (0.0, 0.0, 1.0)
     offset: Union[Array, Tuple[float, float]] = (0.0, 0.0)
     scalar: bool = True
     envelope_waist: float = (1.0,)

src/chromatix/functional/sources.py
@@ -117,23 +117,20 @@ def gaussian_source(
     fourier_spacing = D / shape[0]
     field = create(fourier_spacing, spectrum, spectral_density, shape=shape)
 
-    mask = l2_sq_norm(field.grid) <= 1
-
-    factor = NA**2 / n**2
-
-    sin_theta2 = factor * jnp.sum(field.grid**2 * mask, axis=0)
+    factor = NA / n
+    sin_theta2 = factor**2 * jnp.sum(field.grid**2, axis=0)
     cos_theta = jnp.sqrt(1 - sin_theta2)
     sin_theta = jnp.sqrt(sin_theta2)
 
-    phi = jnp.arctan2(field.grid[0], field.grid[1])
+    phi = jnp.arctan2(factor * field.grid[0], factor * field.grid[1])
     cos_phi = jnp.cos(phi)
     sin_phi = jnp.sin(phi)
     sin_2phi = 2 * sin_phi * cos_phi
     cos_2phi = cos_phi**2 - sin_phi**2
 
     single_field = field.grid[0] ** 2 + field.grid[1] ** 2 <= 1
-    field_x = amplitude[2] * single_field
-    field_y = amplitude[1] * single_field
+    field_x = jnp.complex64(amplitude[2] * single_field)
+    field_y = jnp.complex64(amplitude[1] * single_field)
 
     e_inf_x = ((cos_theta + 1.0) + (cos_theta - 1.0) * cos_2phi) * field_x + (
         cos_theta - 1.0
@@ -150,9 +147,9 @@ def gaussian_source(
 
     offset = _broadcast_1d_to_grid(offset, field.ndim)
     L = jnp.sqrt(field.spectrum * f / n)
-    phase = -jnp.pi * (z / f) * l2_sq_norm(field.grid - offset) / L**2
+    phase = -jnp.pi * (z / f) * l2_sq_norm(factor * field.grid - offset) / L**2
     gaussian_envelope = jnp.exp(
-        -l2_sq_norm(field.grid - offset) / (2 * L**2 * envelope_waist**2)
+        -l2_sq_norm(factor * field.grid - offset) * factor**2 / envelope_waist**2
     )
     u = gaussian_envelope * amplitude * -1j / L**2 * jnp.exp(1j * phase)
     u = jnp.broadcast_to(u, field.shape)